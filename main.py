20cbad78b2c37dbfda4d9f4303827a694632d3c2
M.A <ashrahp0p1@gmail.com>
2023-08-19 22:01:27 +0300

    Form fixed, next stage: portfolio and dark navbar icon fix.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, redirect, url_for
+import os
+
+from flask import Flask, render_template, redirect
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor, CKEditorField
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, SubmitField
 from wtforms.validators import DataRequired
-from flask_ckeditor import CKEditor, CKEditorField
-from flask_bootstrap import Bootstrap5
-import os
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -13,9 +14,10 @@ app.config['SECRET_KEY'] = os.environ['SECRET']
 
 
 class job_req(FlaskForm):
-    name = StringField(validators=[DataRequired()])
-    email = EmailField(validators=[DataRequired()])
-    message = CKEditorField(validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    subject = StringField('Subject', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    message = CKEditorField('Message', validators=[DataRequired()])
     submit = SubmitField(validators=[DataRequired()])
 
 
@@ -23,7 +25,14 @@ class job_req(FlaskForm):
 def home():
     form = job_req()
     if form.validate_on_submit():
-        return redirect(url_for("https://mailto:m.ashraphx@gmail.com"))
+        data = {
+            "Name": form.name.data,
+            "Subject": form.subject.data,
+            "Email": form.email.data,
+            "Message": form.message.data}
+        return redirect(
+            f"mailto:m.ashraphx@gmail.com?subject={data['Subject']} -{data['Name']}&body={data['Message']}\n"
+            f"from: {data['Email']}.")
     return render_template("index.html", form=form)
 
 
